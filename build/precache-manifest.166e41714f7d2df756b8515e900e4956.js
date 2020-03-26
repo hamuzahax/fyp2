9e10d6809d2cbc2cf6e0a23ded4bbb139411635a
hamuzahax <19050719@student.curtin.edu.au>
2020-03-26 13:51:23 +0800

    fourth

---
+++ build/precache-manifest.166e41714f7d2df756b8515e900e4956.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42572b315e04404c94d94ee1ee9cd82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a614d6fcb8fedf074cae",
+    "url": "/static/css/main.496e91dc.chunk.css"
+  },
+  {
+    "revision": "1f506e64957c6b0b5bf6",
+    "url": "/static/js/2.5fba1446.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5fba1446.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a614d6fcb8fedf074cae",
+    "url": "/static/js/main.1bee5df3.chunk.js"
+  },
+  {
+    "revision": "79c9b0a66b9eb2558413",
+    "url": "/static/js/runtime-main.31316f16.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
